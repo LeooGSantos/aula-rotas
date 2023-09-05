a5551a1e4a7e890b54780924859ae93a9d599aae
LeooGSantos <rm98912@fiap.com.br>
2023-09-05 11:07:19 -0300

    alterações na main

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,28 @@ import EditarProdutos from './routes/EditarProdutos/index.jsx'
 
 const router = createBrowserRouter([
     {
-        path: "/"
+        path: "/",
         element: <App/>,
         errorElement: <Error/>,
-        childre
+        children: [
+            {
+                path: "/",
+                element: <Home/>
+            },
+            {
+                path: "/produtos",
+                element: <Produtos/>
+            },
+            {
+                path: "/produtos/editar/:id",
+                element: <EditarProdutos/>
+            }
+        ]
     }
 ])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+)
