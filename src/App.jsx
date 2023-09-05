9c149fce53890f5cc5673dc7bc0dcbf7b58b3363
LeooGSantos <rm98912@fiap.com.br>
2023-09-05 11:26:09 -0300

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
-import {Outlet} from 'reac-router-dom'
+import{Menu} from 'react-router-dom'
+import {Outlet} from 'react-router-dom'
 
 function App(){
 
   return(
     <>
+      <Menu/>
       <Outlet/>
     </>
   )
