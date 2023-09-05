4db228702c76dcce50996632b7cce331114dfa19
LeooGSantos <rm98912@fiap.com.br>
2023-09-05 11:07:08 -0300

    fazendo a main

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,16 @@
 
+
+import { createBrowserRouter, RouterProvider } from 'react-rouder-dom'
+import Home from './routes/Home/index.jsx'
+import Produtos from './routes/Produtos/index.jsx'
+import Error from './routes/Error/index.jsx'
+import EditarProdutos from './routes/EditarProdutos/index.jsx'
+
+const router = createBrowserRouter([
+    {
+        path: "/"
+        element: <App/>,
+        errorElement: <Error/>,
+        childre
+    }
+])
